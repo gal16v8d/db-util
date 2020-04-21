6dfc85b3e204089ebb4cc78d2a14f72f740fccfc
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-04-20 21:36:13 -0500

    Init repo

---
+++ src/main/java/co/com/gsdd/constants/DBConstants.java
@@ -0,0 +1,11 @@
+package co.com.gsdd.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DBConstants {
+
+	public static final String FILE = "file";
+
+}
